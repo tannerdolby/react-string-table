d5b1c72d99d245201b1d2f2d4a8d8c587d099a8c
Tanner <dolb.tanner@gmail.com>
2020-06-08 17:22:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+<img width="933" alt="App image" src="https://user-images.githubusercontent.com/48612525/84092473-542e2200-a9ac-11ea-8eb7-6f03d0fc22f3.png">
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+
+## React String Table
+I created this string variation table originally with HTML, CSS and Vanilla JavaScript. Now this repository holds the converted React project. I was enjoying learning the fundamentals on [freeCodeCamp](https://freeCodeCamp.org) so I thought why not practice my React skills by converting a previous project entirely to React.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.<br />
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br />
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://github.com/tannercd/typescript-pokedex/blob/master/CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Authors
+
+* **Tanner Dolby** - *Initial work* - [@tannercd](https://github.com/tannercd)
+
+## License
+
+This project is free for public and open-source use.
+
---
+++ src/index.js
@@ -0,0 +1,206 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class MyApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputVal: "",
+            strLen: null,
+            strLower: "",
+            strUpper: "",
+            firstChar: "",
+            lastChar: "",
+            titleCase: "",
+            initials: "",
+            strTrim: "",
+            camelCase: "",
+            strASCII: "",
+            strHex: "",
+            rot13: ""
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+    }
+    handleChange(event) {
+        let inputStr = event.target.value;
+        let charArr = inputStr.split(" ");
+        let nameInitials = charArr.map((word) => word.charAt(0).toUpperCase());
+        let titleCased = charArr.map((word) => word.charAt(0).toUpperCase().concat(word.slice(1).toLowerCase())).join(" ");
+        let camelCaseStr = charArr.map((word, index) => {
+            if (index === 0) {
+                return word.toLowerCase();
+            }
+            return word.charAt(0).toUpperCase().concat(word.slice(1).toLowerCase());
+        });
+        let asciiStr = inputStr.split("").map((char) => char.charCodeAt(0)).join(" "); // converting to unicode
+        let hexStr = inputStr.split("").map((char) => char.charCodeAt(0).toString(16)).join(" ");
+        let ROT13 = () => {
+            let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            return inputStr
+                .toUpperCase()
+                .split("")
+                .map((letter) => {
+                    let char = alphabet.indexOf(letter);
+                    return char >= 0 ? alphabet[(char + 13) % 26] : "";
+                })
+                .join("");
+        }
+        let ceasarCipher = ROT13();
+        this.setState({
+            inputVal: inputStr,
+            strLength: inputStr.length,
+            strLower: inputStr.toLowerCase(),
+            strUpper: inputStr.toUpperCase(),
+            firstChar: inputStr.charAt(0),
+            lastChar: inputStr.charAt(inputStr.length - 1),
+            titleCase: titleCased,
+            initials: nameInitials,
+            strTrim: inputStr.trim(),
+            camelCase: camelCaseStr,
+            strASCII: asciiStr,
+            strHex: hexStr,
+            rot13: ceasarCipher   
+        });
+    }
+    handleReset() {
+        this.setState({
+            inputVal: "",
+            strLength: null,
+            strLower: "",
+            strUpper: "",
+            firstChar: "",
+            lastChar: "",
+            titleCase: "",
+            initials: "",
+            strTrim: "",
+            camelCase: "",
+            strASCII: "",
+            strHex: "",
+            rot13: ""
+        });
+    }
+    render() {
+        return (
+            <div>
+                <MyNavComponent />
+                <GetInputComponent input={this.state.inputVal} handleChange={this.handleChange} handleReset={this.handleReset} />
+                <RenderInputComponent input={this.state.inputVal} strLen={this.state.strLength} strLower={this.state.strLower} strUpper={this.state.strUpper} charOne={this.state.firstChar} charLast={this.state.lastChar} initials={this.state.initials} titleCase={this.state.titleCase} trimStr={this.state.strTrim} camelStr={this.state.camelCase} strAscii={this.state.strASCII} strHex={this.state.strHex} ceasarCipher={this.state.rot13}/>
+            </div>
+        );
+    }
+}
+
+class GetInputComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="inputs">
+                <h2>String Variation Table</h2>
+                <p className="input-note">Watch out! There could be state, props or callback functions flying around behind the scenes <i className="fa fa-smile-o" aria-hidden="true" /></p>
+                <div>
+                    <input value={this.props.input} onChange={this.props.handleChange} placeholder="Enter string..." />
+                    <button className="reset" onClick={this.props.handleReset} id="reset-btn">Clear</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+class RenderInputComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="outputs">
+                <table>
+                    <thead>
+                        <tr>
+                            <th width="190px">String Variation</th>
+                            <th>Result</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Original String</td>
+                            <td>{this.props.input}</td>
+                        </tr>
+                        <tr>
+                            <td>String Length</td>
+                            <td>{this.props.strLen}</td>
+                        </tr>
+                        <tr>
+                            <td>Lowercase</td>
+                            <td>{this.props.strLower}</td>
+                        </tr>
+                        <tr>
+                            <td>UpperCase</td>
+                            <td>{this.props.strUpper}</td>
+                        </tr>
+                        <tr>
+                            <td>First Character</td>
+                            <td>{this.props.charOne}</td>
+                        </tr>
+                        <tr>
+                            <td>Last Character</td>
+                            <td>{this.props.charLast}</td>
+                        </tr>
+                        <tr>
+                            <td>Trim String</td>
+                            <td>{this.props.trimStr}</td>
+                        </tr>
+                        <tr>
+                            <td>Acronym Case</td>
+                            <td>{this.props.initials}</td>
+                        </tr>
+                        <tr>
+                            <td>Title Case</td>
+                            <td>{this.props.titleCase}</td>
+                        </tr>
+                        <tr>
+                            <td>camelCase</td>
+                            <td>{this.props.camelStr}</td>
+                        </tr>
+                        <tr>
+                            <td>Ceasar Cipher</td>
+                            <td>{this.props.ceasarCipher}</td>
+                        </tr>
+                        <tr>
+                            <td>Decimal ASCII</td>
+                            <td>{this.props.strAscii}</td>
+                        </tr>
+                        <tr>
+                            <td>Hex format</td>
+                            <td>{this.props.strHex}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+class MyNavComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="navbar">
+                <h1><a href="index.js" aria-label="home page button">StrTable.io</a></h1>
+                <nav>
+                    <ul>
+                        <li><a href="https://github.com/tannercd" target="_blank" rel="noopener noreferrer"><i className="fa fa-github" aria-label="github link"/></a></li>
+                        <li><a href="https://twitter.com/tannercd_" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter" aria-label="twitter link" /></a></li>
+                        <li><a href="https://codepen.io/spherical" target="_blank" rel="noopener noreferrer" aria-label="codepen link"><i className="fa fa-codepen" aria-hidden="false" /></a></li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<MyApp />, document.getElementById("root"));
